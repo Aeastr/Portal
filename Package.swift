b4dde925b5c6f41035081af77c22364333f0f934
Aether <64797587+Aeastr@users.noreply.github.com>
2025-10-08 14:29:39 -0400

    hot fix

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["Portal"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Aeastr/LogOutLoud.git", from: "2.1.1")
+        .package(url: "https://github.com/Aeastr/LogOutLoud.git", from: "2.1.2")
     ],
     targets: [
         .target(
